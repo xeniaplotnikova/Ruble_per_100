ec807a8c3cdb82a5da6e29e40e93e1e60171c807
xeniaplotnikova <60033152+xeniaplotnikova@users.noreply.github.com>
2020-04-06 14:52:07 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,9 @@ public class Main {
     public static void main(String[] args) {
         long amount = 150000;
         long bonus = amount / 100 / 100;
-        System.out.println(bonus);
-        if (amount < 100000) {
-            bonus = 0;
+                if (amount < 100000) {
+             bonus = 0;
         }
+        System.out.println(bonus);            
     }
-
-
 }
